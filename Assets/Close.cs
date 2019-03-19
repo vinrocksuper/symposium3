9bc5a4c56d5a44108da6619b5c11847ff1fb1712
vin <vli8558@bths.edu>
2019-03-19 09:46:42 -0400

    a

---
+++ Assets/Close.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close : MonoBehaviour
+{
+    public GameObject UI;
+    public GameObject UI2;
+
+    private void OnTriggerExit2D()
+    {
+        UI.SetActive(false);
+        UI2.SetActive(false);
+    }
+}
