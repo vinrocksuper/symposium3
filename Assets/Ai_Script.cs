3498b06cddb330ff894497889529409bf9e213e0
BT_1N3_08 <jn>
2019-05-29 10:11:58 -0400

    >:(

--- Assets/Ai_Script.cs
+++ Assets/Ai_Script.cs
@@ -5,23 +5,44 @@ using UnityEngine;
 public class Ai_Script : MonoBehaviour
 {
     public DigitalClock c;
-    public float runSpeed = 300f;
+    public float runSpeed = 1/60000000;
 
     float horizontalMove = 0f;
 
     public Collider2D z;
-
+    private bool m_FacingRight = true;
     // Start is called before the first frame update
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(c.hours == 7 && !c.am)
+        if(c.hours >= 7 && !c.am || c.hours < 6 && c.am || c.hours == 12 && c.am)
         {
-            GetComponent<SpriteRenderer>().enabled = false;
+            if (transform.position.x < 23.13108)
+            {
+                transform.position += new Vector3(runSpeed * Time.deltaTime, 0, 0);
+                if(m_FacingRight)
+                {
+                    Flip();
+                }
+                
+            }
+            //GetComponent<SpriteRenderer>().enabled = false;
         }
+        else
+        {
+            if(transform.position.x > 14.53)
+            {
+                transform.position += new Vector3(-runSpeed * Time.deltaTime, 0, 0);
+                if (!m_FacingRight)
+                {
+                    Flip();
+                }
 
+            }
+           // GetComponent<SpriteRenderer>().enabled = true;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -30,5 +51,16 @@ public class Ai_Script : MonoBehaviour
         {
             Physics2D.IgnoreCollision(collision.collider, z);
         }
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
