2f32bd0f835c60be25dbcb5383eca579d7379c1e
BT_1N3_05 <arafi6624@bths.edu>
2019-06-12 10:13:53 -0400

    indoors

---
+++ Assets/not_destroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class not_destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+}
